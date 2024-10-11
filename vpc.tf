18517ece7a4061d5cc19d56bf52890f8c240ff64
dileepkp334 <dileepkp334@gmail.com>
2024-10-12 00:44:10 +0800

    formatted

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region                  = "us-east-1"
-  access_key              = var.aws_access_key_id
-  secret_key              = var.aws_secret_access_key
+  region     = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 
