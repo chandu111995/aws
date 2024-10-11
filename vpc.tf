04730bfd5aadbc96f6288fa5915250c732da979f
dileepkp334 <dileepkp334@gmail.com>
2024-10-12 01:22:53 +0800

    final updated

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,19 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-}
 
 
 variable "aws_access_key_id" {
   description = "AWS Access Key ID"
-  type        = string
 }
 
 variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
-  type        = string
 }
+
+provider "aws" {
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region     = "us-east-1"
+}
+
 
 
 resource "aws_vpc" "vpc" {
