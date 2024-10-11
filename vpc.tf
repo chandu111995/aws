aaf5998e896f21c935312f057b229976c3494ea8
chandu111995 <candu9007@gmail.com>
2024-10-12 02:18:10 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,7 @@
 
 
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-}
 
 provider "aws" {
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
   region     = "us-east-1"
 }
 
