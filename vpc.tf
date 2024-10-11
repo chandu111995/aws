193d8d46d65109cffcc5ab9347c73fea916a5b93
dileepkp334 <dileepkp334@gmail.com>
2024-10-12 00:41:54 +0800

    added aws keys

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
-
+  region                  = "us-east-1"
+  access_key              = var.aws_access_key_id
+  secret_key              = var.aws_secret_access_key
 }
+
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
+
 
 resource "aws_vpc" "vpc" {
 
